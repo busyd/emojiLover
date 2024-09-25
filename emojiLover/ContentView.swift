4401b5f2290fc6d81931802388ec4c2207a7dae0
Danillo de Faria <dandfaria@gmail.com>
2024-09-25 15:02:01 -0300

    Initial Commit

---
+++ emojiLover/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  emojiLover
+//
+//  Created by Danillo DeFaria on 9/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ emojiLover/emojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  emojiLoverApp.swift
+//  emojiLover
+//
+//  Created by Danillo DeFaria on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct emojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
